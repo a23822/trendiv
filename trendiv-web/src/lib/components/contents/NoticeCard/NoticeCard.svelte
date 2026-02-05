f9ddf9c57a0d1de09dd3c80b7b40912252336faa
chanwoochae <a238220@gmail.com>
2026-02-06 04:02:52 +0900

    fix: noticeCard 버튼 컬러 수정

--- trendiv-web/src/lib/components/contents/NoticeCard/NoticeCard.svelte
+++ trendiv-web/src/lib/components/contents/NoticeCard/NoticeCard.svelte
@@ -24,7 +24,7 @@
 	}
 </script>
 
-{#if isVisible}
+{#if !isVisible}
 	<div
 		transition:slide={{ duration: 300, axis: 'y' }}
 		class={cn(CommonStyles.CARD, 'mb-4 sm:mb-6', 'p-0 sm:p-0')}
@@ -84,6 +84,7 @@
 				onclick={handleClose}
 				size={30}
 				class={cn('absolute top-2 right-2')}
+				variant={'inverted'}
 			/>
 		</div>
 	</div>
